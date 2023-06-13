675cacd17dae7ba3143640c3086e3463028be1a3
Thanakorn Boonyasena <Thanakorn.Boonyasena@gmail.com>
2023-06-13 19:03:14 +0700

    idk

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-$('#my-image').hover(function() {
-    $('#name').addClass('hovered');
-  }, function() {
-    $('#name').removeClass('hovered');
-  });
+$(document).ready(function(){
+    let isBarVisible = false;
   
+    $('#btn').click(function(){
+        if (isBarVisible) {
+            $('#bar').animate({left: '-100%'});
+        } else {
+            $('#bar').animate({left: '0'});
+        }
+        isBarVisible = !isBarVisible;
+    });
+});
